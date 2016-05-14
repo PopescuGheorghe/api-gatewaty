8200539c88c5f6eae504bff4f45ba6e19e54ffce
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2016-05-14 12:48:11 +0300

    added tests for users and sessions

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Api::SessionsController, type: :controller do
+  context 'login' do
+    it 'calls session login' do
+      stub_request(:post, "#{Gateway.base_uri}/api/sessions")
+        .with(body: 'email=email%40example.com&password=password')
+        .to_return(status: 200, body: 'success', headers: {})
+      credentials = { email: 'email@example.com', password: 'password' }
+
+      post :login, credentials
+      expect(response.status).to eql 200
+    end
+
+    context 'logout' do
+      it 'calls user sessions destroy session' do
+        token = '123qwe'
+        stub_request(:delete, "#{Gateway.base_uri}/api/sessions/#{token}")
+          .to_return(status: 200, body: 'success', headers: {})
+
+        delete :logout, id: token
+        expect(response.status).to eq 200
+      end
+    end
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+describe Api::UsersController, type: :controller do
+  before :each do
+    @token = '1234qwe'
+    request.headers['Authorization'] = @token
+  end
+  context 'me' do
+    it 'calls get /api/users/me' do
+      stub_request(:get, "#{Gateway.base_uri}/api/users/me")
+        .with(headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      get :me
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'index' do
+    it 'calls get /api/users' do
+      stub_request(:get, "#{Gateway.base_uri}/api/users")
+        .with(headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'show' do
+    it 'calls get /api/users/:id' do
+      id = 1
+      stub_request(:get, "#{Gateway.base_uri}/api/users/#{id}")
+        .with(headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      get :show, id: id
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'create' do
+    it 'calls post /api/users' do
+      email = 'email@example.com'
+      password = 'password'
+      stub_request(:post, "#{Gateway.base_uri}/api/users")
+        .with(headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      post :create, email: email, password: password
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'update' do
+    it 'calls update /api/users/:id' do
+      email = 'email@example.com'
+      password = 'password'
+      id = 1
+      stub_request(:patch, "#{Gateway.base_uri}/api/users/1")
+        .with(body: 'email=email%40example.com&password=password',
+              headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      patch :update, id: id, email: email, password: password
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'destroy' do
+    it 'calls delete /api/users/:id' do
+      id = 1
+      stub_request(:delete, "#{Gateway.base_uri}/api/users/#{id}")
+        .with(headers: { 'Authorization' => '1234qwe' })
+        .to_return(status: 200, body: '', headers: {})
+
+      delete :destroy, id: id
+      expect(response.status).to eq 200
+    end
+  end
+end
