45cbe8c6ebdf514d52f28f1618eaa8a34124b38a
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2016-06-25 21:31:34 +0300

    added net errors handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,36 @@
 class ApplicationController < ActionController::Base
+  rescue_from  Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+    Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, Errno::ECONNREFUSED,
+    with: :handle_net_errors
+
   respond_to :json
   include Authenticable
+
+  protected
+
+  def handle_net_errors(exception)
+    render json: { success: false, errors: exception.message }
+    return
+  end
+
   class << self
-      Swagger::Docs::Generator::set_real_methods
+    Swagger::Docs::Generator::set_real_methods
 
-      def inherited(subclass)
-        super
-        subclass.class_eval do
-          setup_basic_api_documentation
-        end
+    def inherited(subclass)
+      super
+      subclass.class_eval do
+        setup_basic_api_documentation
       end
+    end
+    private
 
-      private
-      def setup_basic_api_documentation
-        [:me, :index, :show, :create, :update, :destroy, :send_email].each do |api_action|
-          swagger_api api_action do
-            param :header, 'Authorization', :string, :optional, 'Authentication token'
-          end
+
+    def setup_basic_api_documentation
+      [:me, :index, :show, :create, :update, :destroy, :send_email].each do |api_action|
+        swagger_api api_action do
+          param :header, 'Authorization', :string, :optional, 'Authentication token'
         end
       end
     end
+  end
 end
