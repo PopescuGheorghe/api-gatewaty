7ccb6c1854dca7935ddf49ac8b7ce142f9cad45f
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2016-06-25 21:38:34 +0300

    handle network errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from  Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+    Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
+    Net::ProtocolError, Errno::ECONNREFUSED,
+    with: :handle_net_errors
+
   respond_to :json
   include Authenticable
+
+  protected
+
+  def handle_net_errors(exception)
+    render json: { success: false, errors: exception.message }, status: 500
+    return
+  end
 end
